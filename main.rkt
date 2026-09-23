d79249345ad3ad8c66b03754359255247f078fb2
Azrotho <contact@azrotho.fr>
2026-09-23 20:52:41 +0200

    basic bot+update gitignore

--- main.rkt
+++ main.rkt
@@ -4,3 +4,11 @@
 
 (define BOT-TOKEN (getenv "DISCORD_BOT_TOKEN"))
 (define APP-ID (getenv "DISCORD_APP_ID"))
+
+(define client (make-client BOT-TOKEN))
+
+(write "Démarrage du bot Racket...")
+(newline)
+(start-client client)
+(write "OK!")
+(newline)
